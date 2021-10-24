71c1d829b1339280c1aa2d8fc3a90a61bce2402f
thanhhoangggg <93084511+thanhhoangggg@users.noreply.github.com>
2021-10-24 20:50:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Ngọc Thảo';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy Birthday';
 const titleCard = 'Gửi Ngọc Thảo';
-const contentCard = 'Chúc Thảo sinh nhật vui vẻ nha.Đón chào 1 tuổi 18 thật nhiều niềm vui🎂🎂.Ngày càng xinh đẹp😍.Cố gắng học thật giỏi, thật thành công và đạt được ước muốn hay đam mê gì đó. Cái qtrong nữa là chúc Thảo thật hp bên gd và người ấy sau này.';
+const contentCard = 'Chúc Thảo sinh nhật vui vẻ nha.Đón chào 1 tuổi 18 thật nhiều niềm vui🎂🎂.Ngày càng xinh đẹp😍.Cố gắng học thật giỏi, thật thành công và đạt được ước muốn hay đam mê . Cái qtrong nữa là chúc Thảo thật hp bên gd và người ấy sau này. Happy Birthday NT';
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
